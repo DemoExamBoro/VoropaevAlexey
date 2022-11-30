79420620aaeab73bb2b0b4c77f1f5470b16a5f24
DemoExamBoro <119479270+DemoExamBoro@users.noreply.github.com>
2022-11-30 12:16:20 +0300

    Add files via upload
    Voropaev

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoExamenFR
+//
+//  Created by WorldSkills on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
